73122757daf4dcaacda628e78636adde6dff77bb
Fernando97Rocha <contato.dev.fernando@gmail.com>
2024-06-17 12:44:41 -0300

    service lista de desejos implementada

---
+++ Backend/amigosecreto-microservice/lista-desejo-service/src/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes/routes');
+
+const app = express();
+
+app.use(express.json());
+
+app.use(routes);
+
+const username = encodeURIComponent("fernando97");
+const password = encodeURIComponent("crossgnt123")
+
+mongoose.connect(
+    `mongodb+srv://${username}:${password}@amigosecretodb.cusquzn.mongodb.net/?retryWrites=true&w=majority&appName=AmigoSecretoDB`
+);
+
+const connection = mongoose.connection;
+connection.once("open", () => {
+    console.log("MongoBD connected..............")
+})
+
+app.listen(3333, () => {
+    console.log("server is running")
+});
+
+
+
+app.listen(3336);
