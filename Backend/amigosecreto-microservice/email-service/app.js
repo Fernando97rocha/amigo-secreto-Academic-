55572260a7f1201bd129d4787d2ad0356dc024df
Fernando97Rocha <contato.dev.fernando@gmail.com>
2024-06-17 11:27:21 -0300

    serviço de envio de email adicionado

---
+++ Backend/amigosecreto-microservice/email-service/app.js
@@ -0,0 +1,29 @@
+const nodemailer = require('nodemailer');
+
+let remetente = nodemailer.createTransport({
+    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 587,
+    secureConnection: true,
+    auth: {
+        user: 'meuEmail@email.com',
+        pass: 'minhaSenha'
+    }
+})
+
+let emailASerEnviado = {
+    from: 'emailDestino@email.com',
+    to: 'meuEmail@email.com',
+    subject: 'Enviando email com Node.js',
+    text: 'Estou enviando este email com Node.js'
+}
+
+remetente.sendMail(emailASerEnviado, error => {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log('Email enviado com sucesso')
+    }
+})
+
+module.exports = sendMail;
